5c3e4ab345b7595f774efe1451c0c9fa980c506a
ohju96 <ohju96@naver.com>
2022-06-19 02:24:26 +0900

    Setting

---
+++ msa/review-servie/src/test/java/com/example/reviewservie/ReviewServieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reviewservie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
