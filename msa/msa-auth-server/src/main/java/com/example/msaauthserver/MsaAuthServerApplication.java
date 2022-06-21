557b4656d7e0faa899dc3837e52c05e1e114883a
ohju96 <ohju96@naver.com>
2022-06-21 21:05:16 +0900

    gateway Setting

---
+++ msa/msa-auth-server/src/main/java/com/example/msaauthserver/MsaAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.msaauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaAuthServerApplication.class, args);
+    }
+
+}
